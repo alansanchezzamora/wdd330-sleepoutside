b4845803a4035686c14faf1346e9d1e0ce4a1da7
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-01-26 12:17:49 -0700

    rebuild package-lock, switch to product-listings.js (new file) to force netlify to grab it right.

---
+++ src/js/product-listings.js
@@ -0,0 +1,24 @@
+//js for the product-listing html page
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+import { loadHeaderFooter, getParams, capitalizeWord } from './utils.mjs';
+
+//load header/footer wk 3
+loadHeaderFooter();
+
+const category = getParams('category');
+// first create an instance of our ProductData class.
+const dataSource = new ProductData();
+// then get the element we want the product list to render in
+const listElement = document.querySelector('.product-list');
+// then create an instance of our ProductList class and send it the correct information.
+const myList = new ProductList(category, dataSource, listElement);
+
+// finally call the init method to show our products
+myList.init();
+
+//add category name to Top Products on html page & breadcrumb
+const categoryElement = document.getElementById('category-name');
+categoryElement.innerHTML = capitalizeWord(category);
+//also add it to localstorage so it can be used when item is selected.  can't see another way to pull category across to the details page
+localStorage.setItem('category', category);
